9482d52bcaed99c9dc553526e27f03f810778988
channaveer <channaveerpatil999@gmail.com>
2023-10-09 22:56:07 +0530

    challenge chnages

---
+++ 02-number-chanllenges/index.js
@@ -0,0 +1,12 @@
+//generate random number and made some function on it +-/%
+
+let x = Math.ceil(Math.random() * 100 + 1);
+console.log(x);
+let y = Math.ceil(Math.random() * 100 + 1);
+console.log(y);
+
+console.log(x + y);
+console.log(x * y);
+console.log(x / y);
+console.log(x%y);
+
