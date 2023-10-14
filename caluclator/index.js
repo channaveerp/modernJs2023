e4f4f3faa10a42cd8e921eb8f20a70f0e5438db9
channaveer <channaveerpatil999@gmail.com>
2023-10-14 13:15:18 +0530

    switch changes

--- caluclator/index.js
+++ caluclator/index.js
@@ -1,17 +1,39 @@
-// calulator fcntion
-function calulatorFcntion(num1, num2, operator) {
-  if (operator === '+') {
-    return num1 + num2;
-  } else if (operator === '-') {
-    return num1 - num2;
-  } else if (operator === '*') {
-    return num1 * num2;
-  } else if (operator === '/') {
-    return num1 / num2;
-  } else {
-    return 'not valid operator';
+// calulator fcntion using f else
+// function calulatorFcntion(num1, num2, operator) {
+//   if (operator === '+') {
+//     return num1 + num2;
+//   } else if (operator === '-') {
+//     return num1 - num2;
+//   } else if (operator === '*') {
+//     return num1 * num2;
+//   } else if (operator === '/') {
+//     return num1 / num2;
+//   } else {
+//     return 'not valid operator';
+//   }
+// }
+
+// let output = calulatorFcntion(5, 10, '5');
+// console.log(output);
+
+// calulator fcntion using switch operator syntax
+
+function calFucntion(num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '/':
+      return num1 / num2;
+    case '%':
+      return num1 % num2;
+    case '*':
+      return num1 * num2;
+
+    default:
+      return 'not valid operator';
   }
 }
-
-let output = calulatorFcntion(5, 10, '5');
-console.log(output);
+let x = calFucntion(10, 5, '-');
+console.log(x);
